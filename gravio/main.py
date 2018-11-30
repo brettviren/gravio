4a9c4d470b4b48c1b6a833c8b7cc62d9f9d0a568
Brett Viren <brett.viren@gmail.com>
2018-11-30 16:50:09 -0500

    Initial dexnet drawing support.

--- gravio/main.py
+++ gravio/main.py
@@ -38,6 +38,37 @@ def ported_draw(cfg, input_filename, output_filename):
     nx.draw_networkx_labels(G,pos)
     plt.savefig(output_filename)
 
+@cli.command("dexnet")
+@click.argument("input_filename")
+@click.argument("output_filename")
+def dotify_dexnet(input_filename, output_filename):
+    import json
+    dat = json.loads(open(input_filename,'rb').read().decode())
+    import networkx as nx
+    import matplotlib.pyplot as plt
+    from gravio import ported 
+    G = nx.DiGraph()
+    p = ported.Ported(G)
+
+    for agent in dat:
+        agent.setdefault('type', 'agent')
+        nid = "{type}:{name}".format(**agent)
+        p.add_node(nid)
+        for port in agent["ports"]:
+            pid = "port:%s"%port["name"]
+            # fixme: agent ports are not in/out typed
+            p.add_port(nid, pid, ptype='oport')
+            for sock in port.get("bind",[]) + port.get("connect",[]):
+                p.add_node(b);
+                p.connect(pid,sock)
+                
+    pos = nx.spring_layout(G)
+    nx.draw_networkx_nodes(G,pos) 
+    nx.draw_networkx_edges(G,pos) 
+    nx.draw_networkx_labels(G,pos)
+    plt.savefig(output_filename)
+
+
 def main():
     cli(obj=dict())
 
